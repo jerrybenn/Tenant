db029e8029a22d05b8be0118982607d3a3dae24e
Jerry Benn <jerrydarobenn@gmail.com>
2025-10-07 22:52:00 -0400

    Refactor App.js to use double quotes and spacing

--- tenant-admin/src/App.js
+++ tenant-admin/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar';
-import Dashboard from './pages/Dashboard';
-import NotFound from './pages/NotFound';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App() {
-	return (
-		<div className="App">
-			<NavBar />
-			<main style={{ padding: '24px' }}>
-				<Routes>
-					<Route path="/" element={<Dashboard />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</main>
-		</div>
-	);
+  return (
+    <div className="App">
+      <NavBar />
+      <main style={{ padding: "24px" }}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
